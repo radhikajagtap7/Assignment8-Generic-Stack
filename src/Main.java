5b53c271148671b5f354badb18ba0cb8df3ed4fc
Radhika <radhikajagtap60261@gmail.com>
2023-08-26 19:57:10 +0530

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        Stack<String> StackString = new Stack(3);
+
+
+        StackString.push("AA");
+        StackString.push("BB");
+        StackString.push("CC");
+        StackString.push("DD");
+        StackString.display();
+    }
+}
